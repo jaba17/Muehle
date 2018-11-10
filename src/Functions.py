03345a42031ec5ceeb859b508e4167b7472c32d5
jaba17 <jakob.bach15@gmail.com>
2018-11-10 17:25:11 +0100

    added new functions

---
+++ src/Functions.py
@@ -0,0 +1,13 @@
+class Functions:
+
+    def describeBoard(self, mat):
+        board_array = []
+        i = 4
+
+        for r in range(7):
+            for s in range(7):
+                if mat[r][s] == "P" or mat[r][s] == "C":
+                    board_array.append([mat[r][s], r, s])
+                    #   board_array[i] = board_array[i] = [mat[r][s], r, s]
+                    i += 1
+        return board_array
